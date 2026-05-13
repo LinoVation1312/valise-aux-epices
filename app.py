fca9eb7638110367d6d4bfadc1e32c853d642116
Lino Conord <158364911+LinoVation1312@users.noreply.github.com>
2026-05-13 14:28:19 +0200

    Fix markdown formatting in app.py

--- app.py
+++ app.py
@@ -550,7 +550,7 @@ else:
         )
         st.markdown("""
         <p style="font-size:0.60rem; color:#888; margin-top:4px; font-style:italic;">
-            * après déduction de 50 % de crédit d'impôt. Prix initial de 50€.
+            * après déduction de 50 % de crédit d'impôt. Prix initial de 50€.\n
             ** après déduction de 50 % de crédit d'impôt. Prix initial de 30€.
         </p>
         """, unsafe_allow_html=True)
